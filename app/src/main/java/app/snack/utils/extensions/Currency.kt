22eb7e006b1cbc26b5f59dda619af7a2c632576d
Yeghia <yeghia.balayan@gmail.com>
2022-04-13 17:44:57 +0400

    14) Переделать SC. 1000SC = 1$ (округляем до сотых) во всем приложении

--- app/src/main/java/app/snack/utils/extensions/Currency.kt
+++ app/src/main/java/app/snack/utils/extensions/Currency.kt
@@ -3,11 +3,4 @@ package app.snack.utils.extensions
 import java.text.DecimalFormat
 import java.util.*
 
-fun Float.csToUsd(): Double {
-    if (this == 0F) {
-        return 0.00
-    }
-    return DecimalFormat("#,##0.#").format(this / 1000).toDouble()
-}
-
-fun Float.toUsd(decimals: Int = 2) = "$%.${decimals}f".format(Locale.getDefault(), (this.toDouble()/100F))
+fun Float.toUsd(decimals: Int = 2) = "%.${decimals}f$".format(Locale.getDefault(), (this.toDouble()/100F))
